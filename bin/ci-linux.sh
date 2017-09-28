ad4f5bb9c4ae208c315eba034d8187deedbde7a3
Bogdan Tatarov <bogdan.tatarov@gmail.com>
2017-09-29 01:15:13 +0300

    (travis) push changes to travis-osx

--- bin/ci-linux.sh
+++ bin/ci-linux.sh
@@ -6,6 +6,15 @@ UTIL_PATH=$(dirname "${BASH_SOURCE[0]}")
 UTIL_PATH=$(cd $UTIL_PATH/../util; pwd)
 export PATH=$PATH:$UTIL_PATH
 
+echo "Push latest changes to OSX branch"
+git remote add authorigin https://${GH_TOKEN}@github.com/btatarov/moai-sdk.git > /dev/null
+git fetch origin travis-osx
+git checkout travis-osx
+git merge postmorph
+git push origin
+git push authorigin travis-osx
+git checkout postmorph
+
 echo "getting latest cmake"
 pushd ~
 wget http://www.cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.tar.gz --no-check-certificate
