a357a66a88f559915fe7152d02ec5072d9a19b98
Bogdan Tatarov <bogdan.tatarov@gmail.com>
2019-10-31 23:55:45 +0200

    .gitignore for stripped play-services

--- 3rdparty-android/google-play-services-r29/lib/strip.sh
+++ 3rdparty-android/google-play-services-r29/lib/strip.sh
@@ -2,31 +2,34 @@
 
 set -e
 
-GPS="google-play-services.in"
+GPS="google-play-services.jar"
 PRF="com/google/android/gms"
+LIB="google-play-services-stripped.jar"
 
-#rm -rf tmp/
-#mkdir tmp
+rm -rf $LIB || true
+rm -rf tmp || true
+mkdir tmp
 cp $GPS tmp/
 cd tmp/
 
 jar xvf $GPS > /dev/null
-rm -rf "$PRF/fitness"
-rm -rf "$PRF/maps"
-rm -rf "$PRF/nearby"
-rm -rf "$PRF/panorama"
-rm -rf "$PRF/appindexing"
-rm -rf "$PRF/appdatasearch"
-rm -rf "$PRF/drive"
-rm -rf "$PRF/cast"
-rm -rf "$PRF/tagmanager"
-rm -rf "$PRF/measurement"
-rm -rf "$PRF/vision"
-rm -rf "$PRF/wallet"
-rm -rf "$PRF/search"
-rm -rf "$PRF/wearable"
-jar cf google-play-services-stripped.jar com/
-mv google-play-services-stripped.jar ../
+# rm -rf "$PRF/fitness"
+# rm -rf "$PRF/maps"
+# rm -rf "$PRF/nearby"
+# rm -rf "$PRF/panorama"
+# rm -rf "$PRF/appindexing"
+# rm -rf "$PRF/appdatasearch"
+rm -rf "$PRF/drive"  # good first choice - big and deprecated
+# rm -rf "$PRF/cast"
+# rm -rf "$PRF/tagmanager"
+# rm -rf "$PRF/measurement"
+# rm -rf "$PRF/vision"
+# rm -rf "$PRF/wallet"
+# rm -rf "$PRF/search"
+# rm -rf "$PRF/wearable"
+
+jar cf $LIB com/
+mv $LIB ../
 
 cd ..
-#rm -rf tmp/
+rm -rf tmp/
