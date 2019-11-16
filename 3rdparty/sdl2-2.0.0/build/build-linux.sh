87edb7e64d41e06ef3fe9f2f9fa94ed1d042848d
Bogdan Tatarov <bogdan.tatarov@gmail.com>
2019-11-16 13:36:47 +0200

    fix travis automerge

--- 3rdparty/sdl2-2.0.0/build/build-linux.sh
+++ 3rdparty/sdl2-2.0.0/build/build-linux.sh
@@ -10,15 +10,7 @@ mkdir -p "$sdl_root/lib/linux"
 mkdir -p "$sdl_root/lib/build"
 cd $sdl_root/lib/build
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-bash ../../configure --prefix=$sdl_root/lib/build --disable-x11-shared --enable-sdl-dlopen
-=======
 sh ../../configure --prefix=$sdl_root/lib/build --disable-x11-shared --enable-sdl-dlopen
->>>>>>> postmorph
-=======
-sh ../../configure --prefix=$sdl_root/lib/build --disable-x11-shared --enable-sdl-dlopen
->>>>>>> postmorph
 make
 make install
 
