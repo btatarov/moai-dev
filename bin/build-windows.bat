81343a2dc5c38b60cf375b6e62b1c15af3a07a19
Bogdan Tatarov <bogdan.tatarov@gmail.com>
2019-11-16 15:05:03 +0200

    fix vs2012 to vs2013 (windows build)

--- bin/build-windows.bat
+++ bin/build-windows.bat
@@ -10,8 +10,8 @@ if "%arg1%"=="" set arg1=vs2013
 set generator=
 if "%arg1%"=="vs2008" set generator=Visual Studio 9 2008
 if "%arg1%"=="vs2010" set generator=Visual Studio 10
-if "%arg1%"=="vs2012" set generator=Visual Studio 11
-if "%arg1%"=="vs2013" set generator=Visual Studio 12
+if "%arg1%"=="vs2012" set generator=Visual Studio 12
+if "%arg1%"=="vs2013" set generator=Visual Studio 13
 if "%generator%"=="" (
 	@echo Unknown argument "%1". Valid values are vs2008, vs2010, vs2012, vs2013. Exiting.
 	exit /b 1
